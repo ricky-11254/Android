34e323102423394ba14a9974a635ddbe239e6947
ricky-11254 <erickmuchiri11@gmail.com>
2024-03-18 14:32:54 +0300

    code

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,22 +1,25 @@
-﻿using system;
+﻿using System;
 using System.Collections.Generic;
-namespace myNameSpace{
+namespace MyNameSpace{
 	public class Solution{
-		public void TwoSum{
+		public void TwoSum(){
 			int target = int.Parse(Console.ReadLine());
 			int n = int.Parse(Console.ReadLine());
-			int [] nums = new int[n];
-			string input = Console.ReadLine().Split(' ');
+			int[] nums = new int[n];
+			string[] input = Console.ReadLine().Split(' ');
 			for(int i = 0; i < n;i ++){
 				nums[i] = int.Parse(input[i]);
 			}
 			Dictionary <int, int> map = new Dictionary<int, int>();
-			int complement = target - nums[i];
-			if(map.ContainsKey(complement)){
-				Console.WriteLine($"{map[complement]}, {i}");
-			}
-			if(!map.ContainsKey(nums[i])){
-				map.Add(nums[i], [i]);
+			for(int i =0; i< n; i++){
+				int complement = target - nums[i];
+				if(map.ContainsKey(complement)){
+					Console.WriteLine($"{map[complement]}, {i}");
+					break;
+				}
+				if(!map.ContainsKey(nums[i])){
+					map.Add(nums[i], i);
+				}
 			}
 
 		}
